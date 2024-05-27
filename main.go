a5b6817d6530187b9f5dc859e985209f2cded35e
ysnbhb <yassine.bahbib@usmba.ac.ma>
2024-05-27 22:01:11 +0100

    update in code

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	web "web/func" // package creat by team
 )
 
 func main() {
-	http.HandleFunc("/", web.Print)   // if i was in url / use func Print
+	http.HandleFunc("/", web.Print) // if i was in url / use func Print
+	fmt.Println("Server run in http://localhost:8080/")
 	http.ListenAndServe(":8080", nil) // this func for run server
 }
